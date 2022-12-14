4f3bb7e0c7a338cd6c6302786bdf50c30be5a02c
trander123 <trander123@yahoo.com>
2022-12-14 17:51:38 +0800

    added status of About and contact route

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import { ContentWrapper } from "../wrappers/ContentWrapper";
 
 export const About = () => {
-  return <ContentWrapper heading={"About Me"}></ContentWrapper>;
+  return (
+    <ContentWrapper heading={"About Me"}>
+      <h1>Were on the making</h1>
+    </ContentWrapper>
+  );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import { ContentWrapper } from "../wrappers/ContentWrapper";
 
 export const Contact = () => {
-  return <ContentWrapper heading={"Contact"}></ContentWrapper>;
+  return (
+    <ContentWrapper heading={"Contact"}>
+      <h1>Were on the making</h1>
+    </ContentWrapper>
+  );
 };
