a0ad003307fb64ff740a9631adb78d9d5c2af624
trander123 <trander123@yahoo.com>
2023-05-05 07:54:50 +0800

    embeded pdf resume

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,10 @@ export const About = () => {
   const pageDescription = "Here's everything about me.";
 
   return (
-    <ContentWrapper
-      heading={"About Me"}
-      description={pageDescription}
-    ></ContentWrapper>
+    <ContentWrapper heading={"About Me"} description={pageDescription}>
+      <div className=" mb-10">
+        <iframe className="w-96 h-96" src="./files/cv.pdf" title="pdf"></iframe>
+      </div>
+    </ContentWrapper>
   );
 };
